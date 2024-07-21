ae8ff94a3e4b71c446fa876c612d895c711cd431
Marcos Fernando Schiavinatti - FIRST <marcos.schiavinatti@economia.gov.br>
2024-07-20 23:37:01 -0300

    retirada de Class Api Cep não utilizada

--- config/services.php
+++ config/services.php
@@ -30,9 +30,4 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-
-    'cep' => [
-        'endpoint' => \Gabrielmoura\LaravelCep\EndpointOption::POSTMON, // Example: Change to your preferred provider
-    ],
-
 ];
